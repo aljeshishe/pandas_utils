14d7507e5fb881f9431271abfeb3d4650dd00332
alexeygrachev <agrachev@cindicator.com>
2022-12-29 18:51:13 +0400

    removed cli, gui, main

--- pandas_utils/__main__.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-
-"""Package entry point."""
-
-
-from pandas_utils.cli import main
-
-if __name__ == "__main__":  # pragma: no cover
-    main()  # pylint: disable=no-value-for-parameter
